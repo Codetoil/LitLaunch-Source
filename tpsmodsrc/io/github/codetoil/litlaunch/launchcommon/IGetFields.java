8ec1bf7092b1ec0245fcc107f951af204d567f9b
Codetoil <ianthisawesomee@gmail.com>
2019-06-07 06:59:09 -0400

    Update Things

--- tpsmodsrc/io/github/codetoil/litlaunch/launchcommon/IGetFields.java
+++
@@ -1,8 +0,0 @@
-package io.github.codetoil.litlaunch.launchcommon;
-
-public interface IGetFields {
-	long getTotalWorldTime(int dimension);
-	int getDimPlayer();
-	int[] getDimsAvailable();
-	int getDimAmount();
-}
